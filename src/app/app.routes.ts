11a7e455fca85546867c67e22f51a06af0283ecd
yacine <yacinebenz3@gmail.com>
2017-03-10 17:33:26 +0000

    Change title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,3 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 import {SearchComponent} from './search/search.component';
 import {AboutComponent} from './about/about.component';
+
+const Routes = [
+  {path:"", component:SearchComponent},
+  {path:"about", component:AboutComponent}
+]
